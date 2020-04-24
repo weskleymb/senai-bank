105da096b3045f2a9d1d147b80b4057fa68b8e59
Weskley Bezerra <weskleymb@hotmail.com>
2020-04-23 23:20:35 -0300

    ajustes no codigo

--- src/main/java/br/senai/rn/senaibank/controller/HomeController.java
+++ src/main/java/br/senai/rn/senaibank/controller/HomeController.java
@@ -1,12 +1,7 @@
 package br.senai.rn.senaibank.controller;
 
-import br.senai.rn.senaibank.model.Cliente;
-import br.senai.rn.senaibank.model.Conta;
-import br.senai.rn.senaibank.model.Sexo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
--- src/main/java/br/senai/rn/senaibank/model/Conta.java
+++ src/main/java/br/senai/rn/senaibank/model/Conta.java
@@ -1,13 +1,5 @@
 package br.senai.rn.senaibank.model;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import java.util.Objects;
 
 public class Conta {
