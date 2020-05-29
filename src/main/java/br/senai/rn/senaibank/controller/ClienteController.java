037e04ad0626142d22138b67c62da1df731ea982
Weskley Bezerra <weskleymb@hotmail.com>
2020-05-29 14:04:02 -0300

    aula 28/05/2020

--- src/main/java/br/senai/rn/senaibank/controller/ClienteController.java
+++ src/main/java/br/senai/rn/senaibank/controller/ClienteController.java
@@ -1,21 +1,26 @@
 package br.senai.rn.senaibank.controller;
 
 import br.senai.rn.senaibank.model.Cliente;
+import br.senai.rn.senaibank.model.Sexo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/clientes")
 public class ClienteController extends GenericController<Cliente> {
+    @Override
+    public String formulario(Model modelo) {
+        List<Sexo> sexos = Arrays.asList(Sexo.values());
+        modelo.addAttribute("sexos", sexos);
+        return super.formulario(modelo);
+    }
 
-//    @PostMapping(value = "/novo")
-//    public String salvaCliente(Cliente cliente) {
-//        System.out.println("VER AQUI");
-//        service.salva(cliente);
-//        return "redirect:/clientes/lista";
-//    }
-//
-//    @GetMapping(value = "/{id}/editar")
+    //    @GetMapping(value = "/{id}/editar")
 //    public String editar(@PathVariable(value = "id") Long id, Model model) {
 //        Cliente cliente = service.buscaPorId(id);
 //        if (cliente == null) {
@@ -29,13 +34,6 @@ public class ClienteController extends GenericController<Cliente> {
 //        return "clientes/form";
 //    }
 //
-//    @GetMapping(value = "/{id}/remover")
-//    public String remover(@PathVariable(value = "id") Long id) {
-//        Cliente cliente = service.buscaPorId(id);
-//        if (cliente != null) {
-//            service.remover(cliente);
-//        }
-//        return "redirect:/clientes/lista";
-//    }
+
 
 }
--- src/main/java/br/senai/rn/senaibank/controller/GenericController.java
+++ src/main/java/br/senai/rn/senaibank/controller/GenericController.java
@@ -1,12 +1,15 @@
 package br.senai.rn.senaibank.controller;
 
 import br.senai.rn.senaibank.model.AuditableEntity;
-import br.senai.rn.senaibank.model.Cliente;
 import br.senai.rn.senaibank.service.GenericService;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.ParameterizedType;
@@ -16,6 +19,7 @@ public abstract class GenericController<T extends AuditableEntity> {
 
     protected static final String LISTA = "/lista";
     protected static final String FORMULARIO = "/formulario";
+    protected static final String REMOVER = "/{id}/remover";
 
     @Autowired
     protected GenericService<T> service;
@@ -25,6 +29,7 @@ public abstract class GenericController<T extends AuditableEntity> {
         String lista = getNomeEntidadeLista();
         List<T> entidades = service.buscaTodos();
         model.addAttribute(lista, entidades);
+        System.out.println(lista);
         return pagina(LISTA);
     }
 
@@ -34,16 +39,58 @@ public abstract class GenericController<T extends AuditableEntity> {
             String nomeEntidade = getNomeEntidade();
             T entidade = getTipoGenerico().getDeclaredConstructor().newInstance();
             model.addAttribute(nomeEntidade, entidade);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+        } catch (InstantiationException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
             e.printStackTrace();
         }
         return pagina(FORMULARIO);
     }
 
+    @PostMapping
+    public String salva(T entidade) {
+        service.salva(entidade);
+        return redireciona(LISTA);
+    }
+
+    @PostMapping(value = REMOVER)
+    public String remover(@PathVariable(value = "id") Long id) {
+        T entidade = service.buscaPorId(id);
+        if (ObjectUtils.isNotEmpty(entidade)) {
+            service.remover(entidade);
+        }
+        return redireciona(LISTA);
+    }
+
+//    @GetMapping(value = "/{id}/remover")
+//    public String remover(@PathVariable(value = "id") Long id) {
+//        Cliente cliente = service.buscaPorId(id);
+//        if (cliente != null) {
+//            service.remover(cliente);
+//        }
+//        return "redirect:/clientes/lista";
+//    }
+
+    protected String pagina(String pagina) {
+        return getNomeControlador().concat(pagina);
+    }
+
+    protected String redireciona(String url) {
+        return "redirect:"
+                + getMapeamento()
+                + (StringUtils.isEmpty(url) || url.equals(LISTA) ? StringUtils.EMPTY : url);
+    }
+
+    protected String getMapeamento() {
+        return getClass().getAnnotation(RequestMapping.class).value()[0];
+    }
+
     protected String getNomeEntidadeLista() {
         String nomeEntidadeLista = getNomeEntidade();
         String sufixoLista = "Lista";
-        return nomeEntidadeLista + sufixoLista;
+//        return nomeEntidadeLista + sufixoLista;
+        return getMapeamento().replace("/", "");
     }
 
     protected String getNomeEntidade() {
@@ -54,15 +101,10 @@ public abstract class GenericController<T extends AuditableEntity> {
         return (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     }
 
-    protected String pagina(String pagina) {
-        return getNomeControlador().concat(pagina);
-    }
-
     protected String getNomeControlador() {
         String nomeDoControlador = getClass().getSimpleName();
         String sufixoDoControlador = "Controller";
         return StringUtils.uncapitalize(nomeDoControlador.replace(sufixoDoControlador, StringUtils.EMPTY));
     }
-
 
 }
